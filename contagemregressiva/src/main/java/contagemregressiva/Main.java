f9f7089ba4093683fbf46609b8c894b9c1fed9b1
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-04-02 14:02:16 -0300

    Terceiro Commit

--- contagemregressiva/src/main/java/contagemregressiva/Main.java
+++ contagemregressiva/src/main/java/contagemregressiva/Main.java
@@ -3,14 +3,23 @@ package contagemregressiva;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         int seg;
         Scanner input = new Scanner(System.in);
         System.out.println("Insira a quantidade de segundos para iniciar a contagem regressiva: ");
         seg = input.nextInt();
         input.close();
 
+        contarSegundos(seg);
 
+    }
 
+    public static void contarSegundos(int seg) throws InterruptedException
+    {
+        for (int i = seg; i >= 0; i--)
+        {
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
     }
 }
