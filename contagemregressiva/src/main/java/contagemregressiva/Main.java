a13264b1763279f520cc6af79664ad90250ca592
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-04-02 14:04:29 -0300

    Quarto Commit

--- contagemregressiva/src/main/java/contagemregressiva/Main.java
+++ contagemregressiva/src/main/java/contagemregressiva/Main.java
@@ -19,6 +19,11 @@ public class Main {
         for (int i = seg; i >= 0; i--)
         {
             System.out.println(i);
+            if (i == 0)
+            {
+                System.out.println("Contagem regressiva finalizada!");
+                break;
+            }
             Thread.sleep(1000);
         }
     }
